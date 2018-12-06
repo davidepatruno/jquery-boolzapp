c4a8eed2a3fa5723cb2bb70b53c1c12e591ddda1
davidepatruno <patrunodavid@gmail.com>
2018-12-06 10:57:13 +0100

    invio messaggio fatto

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-// $(document).ready(function() {
-//   $('#inputmessaggio').keypress(function(event){
-//     if(event.which == 13){
-//       var testomessaggio = $('#inputmessaggio').val();
-//       var templateMessaggio = $('.temp_messaggio');
-//       $('.chat_wrapper').append(templateMessaggio);
-//       console.log(templateMessaggio);
-//
-//     }
-//   })
-// })
-
-// per provare questa strada hai modificato gli attributi alla classe .user_message, se non va ricorda di ripristinare il displayflex(gia in html).   
-
-$(document).ready(function(){
+$(document).ready(function() {
   $('#inputmessaggio').keypress(function(event){
-    if(event.which == 13) {
-      var testomessaggio = $('#inputmessaggio').val();
-      var templateMessaggio = $('.temp_messaggio');
-      templateMessaggio.text(testomessaggio);
-      var divsend = $("<div class='user_message new'></div>");
-      $('.chat_wrapper').append(divsend);
-      $('.new').append(templateMessaggio);
+    if(event.which == 13){
+      var thisinput = $(this);
+      var testomessaggio = thisinput.val();
+      var now = new Date();
+      if (testomessaggio) {
+        var templateMessaggio = $('.template .message').clone();
+        templateMessaggio.children('.mess_text').text(testomessaggio);
+        templateMessaggio.children('.mess_time').text(now.getHours() + ':'+ now.getMinutes());
+        $('.chat_body').append(templateMessaggio);
+
+        var clearTemp = $('.template .clear').clone();
+        $('.chat_body').append(clearTemp);
+      }
+      $('#inputmessaggio').val('');
     }
   })
-
 })
