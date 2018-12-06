cf608269e58b79e3b0428fef53edc1f82d69f359
davidepatruno <patrunodavid@gmail.com>
2018-12-06 17:39:36 +0100

    search milestone started

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
   $('#inputmessaggio').keypress(function(event){
+
     if(event.which == 13){
       var thisinput = $(this);
       var testomessaggio = thisinput.val();
       var now = new Date();
-      if (testomessaggio) {
-          var templateMessaggio = $('.template .messagesent').clone();
-          templateMessaggio.children('.mess_text').text(testomessaggio);
-          templateMessaggio.children('.mess_time').text(now.getHours() + ':' + now.getMinutes());
-          $('.chat_body').append(templateMessaggio);
 
+      if (testomessaggio) {
+
+        var templateMessaggio = $('.template .messagesent').clone();
+        templateMessaggio.children('.mess_text').text(testomessaggio);
+        templateMessaggio.children('.mess_time').text(now.getHours() + ':' + (now.getMinutes()<10?'0':'') + now.getMinutes());
+        $('.chat_body').append(templateMessaggio);
+
+        var clearTemp = $('.template .clear').clone();
+        $('.chat_body').append(clearTemp);
+
+        var answ_time = setTimeout(function() {
+
+          var templateRisposta = $('.template .messagereceived').clone();
+          templateRisposta.children('.mess_text').text('ok');
+          templateRisposta.children('.mess_time').text(now.getHours() + ':' + (now.getMinutes()<10?'0':'') + now.getMinutes());
+          $('.chat_body').append(templateRisposta);
+          
           var clearTemp = $('.template .clear').clone();
           $('.chat_body').append(clearTemp);
-
-          var answ_time = setTimeout(function() {
-            var templateRisposta = $('.template .messagereceived').clone();
-            templateRisposta.children('.mess_text').text('ok');
-            templateRisposta.children('.mess_time').text(now.getHours() + ':' + now.getMinutes());
-            $('.chat_body').append(templateRisposta);
-            var clearTemp = $('.template .clear').clone();
-            $('.chat_body').append(clearTemp);
-          }, 5000);
+        }, 5000);
       }
       $('#inputmessaggio').val('');
-
     }
   })
 })
